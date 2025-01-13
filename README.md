686b07f7de3aecf44da78f80defe26549e96ab16
Martin Aviles <maviles2001@gmail.com>
2025-01-13 10:36:39 +0100

    init

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # DataSystemsProjectF1
+
+Data Systems Project Group F1 2024/2025 
+
+Making a product for a company that shall not be named
---
+++ data.py
@@ -0,0 +1,24 @@
+ENTITY_TYPES = [
+    "Contractor",
+    "ConstructionProject",
+    "Location",
+    "Material",
+    "Supplier",
+    "LightBulbModel",
+    "ConcreteType",
+    "SteelType",
+    "WoodType"
+]
+RELATIONSHIP_TYPES = [
+    "constructed",
+    "locatedAt",
+    "usesMaterial",
+    "suppliedBy",
+    "managedBy",
+    "sourcedFrom",
+    "employs",
+    "containsLightBulbModel",
+    "containsConcreteType",
+    "containsSteelType",
+    "containsWoodType"
+]
